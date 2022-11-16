382695ee33bb59eb443277980a939591c5537e89
fianouse <115932730+fianouse@users.noreply.github.com>
2022-11-16 18:42:23 +0100

    Add files via upload

---
+++ Data/candidaturedb.cs
@@ -0,0 +1,18 @@
+﻿using last_test_tech.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace last_test_tech.Data
+{
+    public class candidaturedb:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=Last_DB;Trusted_Connection=True;");
+        }
+        public DbSet<Candidat> candidatures { get; set; }
+    }
+}
---
+++ Migrations/20221116160404_InitialMigration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace last_test_tech.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "CD");
+
+            migrationBuilder.CreateTable(
+                name: "Candidatures",
+                schema: "CD",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nom = table.Column<string>(type: "varchar(50)", nullable: false),
+                    prenom = table.Column<string>(type: "varchar(50)", nullable: false),
+                    mail = table.Column<string>(type: "varchar(200)", nullable: false),
+                    tele = table.Column<int>(type: "int", nullable: false),
+                    niveau_detude = table.Column<string>(type: "varchar(25)", nullable: false),
+                    nbr_anne_exp = table.Column<int>(type: "int", nullable: false),
+                    dn_employeur = table.Column<string>(type: "varchar(50)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Candidatures", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Candidatures",
+                schema: "CD");
+        }
+    }
+}
